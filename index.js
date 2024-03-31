4358ab4045e889906feb509b615fae2fa64cb61e
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-03-31 22:12:28 +0900

    UTF-16のコードで「おなかすいた」と表示させた

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ console.log("/////")
 console.log("start")
 console.log("/////")
 
-const good = "こんにちは"
-console.log(good);
+
+
+// UTF-16のコードで「おなかすいた」と表示させたい。文字コードはChatGPTで聞きました
+console.log(String.fromCharCode(0x304a, 0x306a, 0x304b, 0x3059, 0x3044, 0x305f));
