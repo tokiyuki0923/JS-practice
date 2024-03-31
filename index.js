217e516f79f02600a6d7a437a78ba7ade00ac8c9
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-03-31 23:31:45 +0900

    String.rawで文字列をそっくりそのまま出力する

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ console.log(String.fromCharCode(0x304a, 0x306a, 0x304b, 0x3059, 0x3044, 0x305f))
 
 //Unicodeコードポイントを用いて「おなかいっぱい」と表示させたい。expected output:おなかいっぱい
 console.log(String.fromCodePoint(0x304a, 0x306a, 0x304b, 0x3044, 0x3063, 0x3071, 0x3044));
+
+
+
+
+//「\n」が含まれていると改行されてしまう。出力したい文字列に\nが含まれているが、そのまんま全く同じ文字列を出力したい。
+const aiue = String.raw`C:\Users\node\index.js`;
+console.log(aiue);
+// ちなみにStringをしようしないと\nのところで改行されて出力される
+const ue = "C:\Users\node\index.js";
+console.log(ue);
