cdd1d14d663dcf81527b1c923fe0a04944e1fc23
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-04-04 21:28:07 +0900

    アロー関数

--- index.js
+++ index.js
@@ -283,4 +283,15 @@ function edd(x, y) {
     return x + y;
 }
 edd(1, 3);
-edd(1, 3, 5); 
+edd(1, 3, 5);
+
+// アロー関数のルール
+// 仮引数の数と定義
+const fnA = () => { /* 仮引数がないとき */ };
+const fnB = (x) => { /* 仮引数が1つのみのとき */ };
+const fnC = x => { /* 仮引数が1つのみのときは()を省略可能 */ };
+const fnD = (x, y) => { /* 仮引数が複数のとき */ };
+// 値の返し方
+// 次の２つの定義は同じ意味となる
+const mulA = x => { return x * x; }; // ブロックの中でreturn
+const mulB = x => x * x;            // 1行のみの場合はreturnとブロックを省略できる
